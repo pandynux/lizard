e4c701c2d3b5f2509b7a94cfd85bd9c496bea83c
novaeco <novatribolnovea@gmail.com>
2025-07-11 11:25:33 +0200

    Remove unused LovyanGFX placeholder

--- main/display_drivers/lovyangfx_driver.c
+++
@@ -1,6 +0,0 @@
-#include "board_config.hpp"
-// ... include LovyanGFX headers
-
-void display_init(int width, int height, ...) {
-    // Initialisation LovyanGFX selon les broches/params du board_config.hpp
-}

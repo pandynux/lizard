e44dfb0e624987c9edbd4b877271e15506e1dffa
pandynux <99563475+pandynux@users.noreply.github.com>
2025-08-24 13:48:15 +0200

    Update board_7inch.hpp

--- main/board/board_7inch.hpp
+++ main/board/board_7inch.hpp
@@ -1,14 +1,18 @@
 #pragma once
-#define BOARD_NAME "Waveshare ESP32-S3-Touch-LCD-7"
-#define LCD_WIDTH  1024
-#define LCD_HEIGHT 600
-#define LCD_MOSI   47
-#define LCD_CLK    21
-#define LCD_CS     15
-#define LCD_DC     14
-#define LCD_RST    13
-#define LCD_BL     2
-#define TOUCH_SDA  18
-#define TOUCH_SCL  8
-#define TOUCH_INT  16
-#define GT911_I2C_ADDR 0x5D
+
+// Utilisation de 'static constexpr' pour les constantes en C++.
+namespace board_7inch {
+    static constexpr const char* BOARD_NAME = "Waveshare ESP32-S3-Touch-LCD-7";
+    static constexpr int LCD_WIDTH  = 1024;
+    static constexpr int LCD_HEIGHT = 600;
+    static constexpr int LCD_MOSI   = 47;
+    static constexpr int LCD_CLK    = 21;
+    static constexpr int LCD_CS     = 15;
+    static constexpr int LCD_DC     = 14;
+    static constexpr int LCD_RST    = 13;
+    static constexpr int LCD_BL     = 2;
+    static constexpr int TOUCH_SDA  = 18;
+    static constexpr int TOUCH_SCL  = 8;
+    static constexpr int TOUCH_INT  = 16;
+    static constexpr int GT911_I2C_ADDR = 0x5D;
+}
